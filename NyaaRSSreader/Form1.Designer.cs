2abd64f028f7ba78de2212da159cc17b0b62bb99
vi000246 <vi000246@gmail.com>
2016-10-17 15:45:17 +0800

    213

--- NyaaRSSreader/Form1.Designer.cs
+++ NyaaRSSreader/Form1.Designer.cs
@@ -28,19 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.cbRssCate = new System.Windows.Forms.ComboBox();
             this.lbRssCate = new System.Windows.Forms.Label();
             this.btnLoadRSS = new System.Windows.Forms.Button();
             this.lbTorrPath = new System.Windows.Forms.Label();
             this.textPath = new System.Windows.Forms.TextBox();
             this.btnBrowse = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Title = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Size = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Seeder = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Leecher = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.download = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.articleLink = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DownloadLink = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnView = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.btnDownload = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // cbRssCate
             // 
             this.cbRssCate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbRssCate.FormattingEnabled = true;
-            this.cbRssCate.Location = new System.Drawing.Point(12, 22);
+            this.cbRssCate.Location = new System.Drawing.Point(3, 29);
             this.cbRssCate.Name = "cbRssCate";
             this.cbRssCate.Size = new System.Drawing.Size(121, 20);
             this.cbRssCate.TabIndex = 0;
@@ -48,7 +65,7 @@
             // lbRssCate
             // 
             this.lbRssCate.AutoSize = true;
-            this.lbRssCate.Location = new System.Drawing.Point(12, 9);
+            this.lbRssCate.Location = new System.Drawing.Point(3, 11);
             this.lbRssCate.Name = "lbRssCate";
             this.lbRssCate.Size = new System.Drawing.Size(97, 12);
             this.lbRssCate.TabIndex = 1;
@@ -56,18 +73,18 @@
             // 
             // btnLoadRSS
             // 
-            this.btnLoadRSS.Location = new System.Drawing.Point(158, 19);
+            this.btnLoadRSS.Location = new System.Drawing.Point(130, 26);
             this.btnLoadRSS.Name = "btnLoadRSS";
             this.btnLoadRSS.Size = new System.Drawing.Size(75, 23);
             this.btnLoadRSS.TabIndex = 2;
-            this.btnLoadRSS.Text = "載入RSS";
+            this.btnLoadRSS.Text = "搜尋";
             this.btnLoadRSS.UseVisualStyleBackColor = true;
             this.btnLoadRSS.Click += new System.EventHandler(this.btnLoadRSS_Click);
             // 
             // lbTorrPath
             // 
             this.lbTorrPath.AutoSize = true;
-            this.lbTorrPath.Location = new System.Drawing.Point(12, 45);
+            this.lbTorrPath.Location = new System.Drawing.Point(3, 54);
             this.lbTorrPath.Name = "lbTorrPath";
             this.lbTorrPath.Size = new System.Drawing.Size(89, 12);
             this.lbTorrPath.TabIndex = 1;
@@ -75,35 +92,146 @@
             // 
             // textPath
             // 
-            this.textPath.Location = new System.Drawing.Point(14, 61);
+            this.textPath.Location = new System.Drawing.Point(3, 75);
             this.textPath.Name = "textPath";
             this.textPath.Size = new System.Drawing.Size(119, 22);
             this.textPath.TabIndex = 3;
             // 
             // btnBrowse
             // 
-            this.btnBrowse.Location = new System.Drawing.Point(158, 59);
+            this.btnBrowse.Location = new System.Drawing.Point(130, 73);
             this.btnBrowse.Name = "btnBrowse";
             this.btnBrowse.Size = new System.Drawing.Size(75, 23);
             this.btnBrowse.TabIndex = 4;
             this.btnBrowse.Text = "瀏覽";
             this.btnBrowse.UseVisualStyleBackColor = true;
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Title,
+            this.Size,
+            this.Seeder,
+            this.Leecher,
+            this.download,
+            this.articleLink,
+            this.DownloadLink,
+            this.Date,
+            this.btnView,
+            this.btnDownload});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 103);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(839, 221);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // Title
+            // 
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("新細明體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Black;
+            this.Title.DefaultCellStyle = dataGridViewCellStyle1;
+            this.Title.HeaderText = "標題";
+            this.Title.Name = "Title";
+            // 
+            // Size
+            // 
+            this.Size.HeaderText = "檔案大小";
+            this.Size.Name = "Size";
+            this.Size.ReadOnly = true;
+            // 
+            // Seeder
+            // 
+            this.Seeder.HeaderText = "Seeders";
+            this.Seeder.Name = "Seeder";
+            this.Seeder.ReadOnly = true;
+            // 
+            // Leecher
+            // 
+            this.Leecher.HeaderText = "Leechers";
+            this.Leecher.Name = "Leecher";
+            this.Leecher.ReadOnly = true;
+            // 
+            // download
+            // 
+            this.download.HeaderText = "Downloads";
+            this.download.Name = "download";
+            this.download.ReadOnly = true;
+            // 
+            // articleLink
+            // 
+            this.articleLink.HeaderText = "文章連結";
+            this.articleLink.Name = "articleLink";
+            this.articleLink.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.articleLink.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.articleLink.Visible = false;
+            // 
+            // DownloadLink
+            // 
+            this.DownloadLink.HeaderText = "下載連結";
+            this.DownloadLink.Name = "DownloadLink";
+            this.DownloadLink.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.DownloadLink.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.DownloadLink.Visible = false;
+            // 
+            // Date
+            // 
+            this.Date.HeaderText = "發佈日期";
+            this.Date.Name = "Date";
+            this.Date.ReadOnly = true;
+            // 
+            // btnView
+            // 
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.NullValue = "預覽圖";
+            this.btnView.DefaultCellStyle = dataGridViewCellStyle2;
+            this.btnView.HeaderText = "文章連結";
+            this.btnView.Name = "btnView";
+            this.btnView.Text = "預覽圖";
+            this.btnView.UseColumnTextForButtonValue = true;
+            // 
+            // btnDownload
+            // 
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.Transparent;
+            dataGridViewCellStyle3.NullValue = "下載";
+            this.btnDownload.DefaultCellStyle = dataGridViewCellStyle3;
+            this.btnDownload.HeaderText = "下載連結";
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Text = "下載";
+            this.btnDownload.UseColumnTextForButtonValue = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnLoadRSS);
+            this.panel1.Controls.Add(this.btnBrowse);
+            this.panel1.Controls.Add(this.lbTorrPath);
+            this.panel1.Controls.Add(this.textPath);
+            this.panel1.Controls.Add(this.cbRssCate);
+            this.panel1.Controls.Add(this.lbRssCate);
+            this.panel1.Location = new System.Drawing.Point(14, -3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(586, 100);
+            this.panel1.TabIndex = 6;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.btnBrowse);
-            this.Controls.Add(this.textPath);
-            this.Controls.Add(this.btnLoadRSS);
-            this.Controls.Add(this.lbTorrPath);
-            this.Controls.Add(this.lbRssCate);
-            this.Controls.Add(this.cbRssCate);
+            this.ClientSize = new System.Drawing.Size(863, 336);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -115,6 +243,18 @@
         private System.Windows.Forms.Label lbTorrPath;
         private System.Windows.Forms.TextBox textPath;
         private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Title;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Size;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Seeder;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Leecher;
+        private System.Windows.Forms.DataGridViewTextBoxColumn download;
+        private System.Windows.Forms.DataGridViewTextBoxColumn articleLink;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DownloadLink;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Date;
+        private System.Windows.Forms.DataGridViewButtonColumn btnView;
+        private System.Windows.Forms.DataGridViewButtonColumn btnDownload;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- NyaaRSSreader/Form1.cs
+++ NyaaRSSreader/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
@@ -17,10 +18,14 @@ namespace NyaaRSSreader
         public Form1()
         {
             InitializeComponent();
+
             //綁定下拉選單的物件
             BindComboboxData();
-        }
 
+        }
+        /// <summary>
+        /// 下拉選單的值
+        /// </summary>
         public void BindComboboxData(){
             Dictionary<string, string>test = new Dictionary<string, string>();
             test.Add("https://sukebei.nyaa.se/?page=rss&cats=8_0", "R18-RealLife");
@@ -41,23 +46,63 @@ namespace NyaaRSSreader
             //新建XML文件類別
              XDocument myXDoc = XDocument.Load(cbRssCate.SelectedValue.ToString());
              var rootNode = myXDoc.Root.Element("channel").Descendants("item");
+            //取得每個文章的資料
             foreach (var item in rootNode)
             {
                 string title = item.Element("title").Value;  // 標題
                 string link = item.Element("link").Value;    // 下載連結
                 string guid = item.Element("guid").Value;    // 文章連結
-                string description = item.Element("description").Value;    // 作種者 下載者個數
                 string pubDate = item.Element("pubDate").Value;    // 發佈日期
+                string description = item.Element("description").Value;    // 作種者 下載者個數
+
+                //過濾出需要的資訊
+                Regex pattern1 = new Regex(
+@"(?<seeder>\d+)\s+seeder\(s\),\s+(?<leecher>\d+)\s+leecher\(s\),\s+(?<download>\d+)\s+download\(s\)\s-\s+(?<size>[\d.\w\W]+)"
+                , RegexOptions.Singleline);
+                Match match = pattern1.Match(description);
+
+                string Download = match.Groups["download"].Value;
+                string Size = match.Groups["size"].Value;
+                string Leecher = match.Groups["leecher"].Value;
+                string Seeder = match.Groups["seeder"].Value;
+                
+                this.dataGridView1.Rows.Add(title, Size, Seeder, Leecher, Download, guid, link, pubDate);
             }
 
 
 
         }
 
-
+        /// <summary>
+        /// 按下載入Rss按鈕
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnLoadRSS_Click(object sender, EventArgs e)
         {
             XmlStringToDataTable();
+        }
+
+        /// <summary>
+        /// Row Cell Button Click Event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow row = this.dataGridView1.Rows[e.RowIndex];
+            
+            //如果按下的是預覽按鈕
+            if (e.ColumnIndex == dataGridView1.Columns["btnView"].Index && e.RowIndex >= 0)
+            {
+                MessageBox.Show(row.Cells["articleLink"].Value.ToString());
+            }
+
+            //如果按下的是下載按鈕
+            if (e.ColumnIndex == dataGridView1.Columns["btnDownload"].Index && e.RowIndex >= 0)
+            {
+                MessageBox.Show(row.Cells["DownloadLink"].Value.ToString());
+            }
         }
 
     }
