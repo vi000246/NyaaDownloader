55eb26a3ed5c5b7c30410e2c7fd1f75adcee454c
vi000246 <vi000246@gmail.com>
2016-10-19 10:03:50 +0800

    123

--- NyaaRSSreader/GetPreViewImage.cs
+++ NyaaRSSreader/GetPreViewImage.cs
@@ -36,12 +36,17 @@ namespace NyaaRSSreader
             List<string> BigImageList = new List<string>();
             List<string> SmallImageList = new List<string>();
 
+            //只取出html裡描述的部份 其他去掉
+
+
             #region 圖床判斷1 small換big 刪掉_thumb
             /*
              * 圖床清單:
              * imgdream,imgblank,img.yt,dimtus.com,imgstudio
              * damimage,imgseed,55888,imageteam,imagedecode,
-             * hentai,imgchili
+             * hentai,imgchili,
+             * 以下兩個是直接開圖就好
+             * tinypic,pics.dmm.co.jp
              * 
              * 邏輯:
              * 以上的圖床，將small或_thumb替換掉就能顯示大圖
@@ -50,7 +55,7 @@ namespace NyaaRSSreader
             //分離出html裡的image Url
             Regex pattern = new Regex(
                 //p.s. ?:是關閉括號的capture功能
-            @"(?<url>https?://[\d\w-_.]*(?:imgdream|imgblank|img.yt|dimtus|imgstudio|damimage|imgseed|55888|imageteam|imagedecode|hentai|imgchili)(?:[\d\w-_./]*)[\d\w-_.]*.jpe?g)"
+            @"(?<url>https?://[\d\w-_.]*(?:imgdream|imgblank|img.yt|dimtus|imgstudio|damimage|imgseed|55888|imageteam|imagedecode|hentai|tinypic|pics.dmm)(?:[\d\w-_./]*)[\d\w-_.]*.jpe?g)"
             , RegexOptions.Multiline);
             MatchCollection matchGuides = pattern.Matches(html);
             foreach (Match guide in matchGuides)
@@ -62,6 +67,27 @@ namespace NyaaRSSreader
             foreach (var strUrl in SmallImageList)
             {
                 string newUrl = strUrl.Replace("small", "big").Replace("_thumb", "");
+                BigImageList.Add(newUrl);
+            }
+            #endregion
+
+            #region 判斷imgchili
+            SmallImageList.Clear();
+            Regex pattern3 = new Regex(
+                //p.s. ?:是關閉括號的capture功能
+            @"(?<url>https?://[\d\w-_.]*(?:imgchili)(?:[\d\w-_./]*)[\d\w-_.]*.jpe?g)"
+            , RegexOptions.Multiline);
+            MatchCollection matchGuides3 = pattern3.Matches(html);
+            foreach (Match guide in matchGuides3)
+            {
+                SmallImageList.Add(guide.Groups["url"].ToString());
+            }
+
+            //判斷能否取到大圖 
+            foreach (var strUrl in SmallImageList)
+            {
+                //將t10、t6、t100之類的字串換成i10、i6、i100
+                string newUrl = Regex.Replace(strUrl, @"t(\d{0,3})\.", m => "i" + m.Groups[1].Value);
                 BigImageList.Add(newUrl);
             }
             #endregion
--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -21,6 +21,18 @@ namespace UnitTest
             }
         }
 
+        [TestMethod]
+        //判斷imgchili能不能傳回大圖
+        public void TestImgchili() {
+            //輸入小圖的網址 
+            var urlList = new GetPreViewImage().GetBigImageUrl("http://t10.imgchili.net/90670/90670491_nktv_007.jpg");
+            Assert.IsTrue(urlList.Count > 0);
+            foreach (var url in urlList)
+            {
+                Assert.IsTrue(Regex.IsMatch(url, @"(?<url>https?://i\d{0,3}.imgchili.net(?:[\d\w-_./\?]*)[\d\w-_.]*.jpe?g)", RegexOptions.Singleline));
+            }
+        }
+
 
     }
 }
